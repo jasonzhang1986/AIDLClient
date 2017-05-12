fc8216c992f71026aee7a3ab84bf9b3440717ac0
zhangjifeng <zhangjifeng@le.com>
2017-05-12 17:59:56 +0800

    设置binder的死亡代理

--- app/src/main/java/me/jifengzhang/aidlclient/MainActivity.java
+++ app/src/main/java/me/jifengzhang/aidlclient/MainActivity.java
@@ -21,10 +21,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent("me.jifengzhang.aidl.Remote");
-        intent.setPackage("me.jifengzhang.aidlserver"); //Fixed:Service Intent must be explicit
-
-        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        bindRemote();
 
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,12 +44,24 @@ public class MainActivity extends Activity {
         unbindService(mConnection);
     }
 
+    private void bindRemote() {
+        Intent intent = new Intent("me.jifengzhang.aidl.Remote");
+        intent.setPackage("me.jifengzhang.aidlserver"); //Fixed:Service Intent must be explicit
+
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
     private IRemoteService mService;
     private ServiceConnection mConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
             Log.i("AIDLDemo","onServiceConnected");
             mService = IRemoteService.Stub.asInterface(service);
+            try {
+                service.linkToDeath(deathRecipient,0);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
         }
 
         @Override
@@ -62,5 +71,17 @@ public class MainActivity extends Activity {
         }
     };
 
-
+    private final IBinder.DeathRecipient deathRecipient = new IBinder.DeathRecipient() {
+        @Override
+        public void binderDied() {
+            Log.i("AIDLDemo","binderDied");
+            if (mService==null) {
+                return;
+            }
+            mService.asBinder().unlinkToDeath(deathRecipient, 0);
+            mService = null;
+            //重新绑定服务
+            bindRemote();
+        }
+    };
 }
