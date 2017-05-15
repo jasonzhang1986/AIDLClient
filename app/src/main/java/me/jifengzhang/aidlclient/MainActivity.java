417de15373d4c6a5520a8f24636621e082649dd0
zhangjifeng <zhangjifeng@le.com>
2017-05-15 12:58:56 +0800

    BinderPool连接池，避免每个aidl都需要一个Service

--- app/src/main/java/me/jifengzhang/aidlclient/MainActivity.java
+++ app/src/main/java/me/jifengzhang/aidlclient/MainActivity.java
@@ -1,35 +1,34 @@
 package me.jifengzhang.aidlclient;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 
-import me.jifengzhang.aidlserver.IRemoteService;
-import me.jifengzhang.aidlserver.RemoteData;
+import me.jifengzhang.aidlserver.BinderPool;
+import me.jifengzhang.aidlserver.ICalculate;
+import me.jifengzhang.aidlserver.ICalculateImpl;
+
 
 public class MainActivity extends Activity {
+
+    private BinderPool mBinderPool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        bindRemote();
+        getBinderPool();
 
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mService!=null) {
+                if(mBinderPool!=null) {
                     try {
-                        RemoteData data = new RemoteData(1, "Android");
-                        mService.addData(data);
+                        ICalculate binder = ICalculateImpl.asInterface(mBinderPool.queryBinder(BinderPool.BINDER_CALCULATE));
+                        int result = binder.add(1,2);
+                        Log.i("AIDLDemo", "calculate add = " + result);
                     } catch (RemoteException e) {
                         e.printStackTrace();
                     }
@@ -38,50 +37,14 @@ public class MainActivity extends Activity {
         });
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unbindService(mConnection);
+    private void getBinderPool() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mBinderPool = BinderPool.getInstance(MainActivity.this);
+            }
+        }).start();
     }
 
-    private void bindRemote() {
-        Intent intent = new Intent("me.jifengzhang.aidl.Remote");
-        intent.setPackage("me.jifengzhang.aidlserver"); //Fixed:Service Intent must be explicit
 
-        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-    }
-
-    private IRemoteService mService;
-    private ServiceConnection mConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            Log.i("AIDLDemo","onServiceConnected");
-            mService = IRemoteService.Stub.asInterface(service);
-            try {
-                service.linkToDeath(deathRecipient,0);
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            Log.i("AIDLDemo","onServiceDisconnected");
-            mService = null;
-        }
-    };
-
-    private final IBinder.DeathRecipient deathRecipient = new IBinder.DeathRecipient() {
-        @Override
-        public void binderDied() {
-            Log.i("AIDLDemo","binderDied");
-            if (mService==null) {
-                return;
-            }
-            mService.asBinder().unlinkToDeath(deathRecipient, 0);
-            mService = null;
-            //重新绑定服务
-            bindRemote();
-        }
-    };
 }
